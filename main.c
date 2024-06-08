75700033a5a354c2263b578566ed1f04cdd35eac
bayanaka <130218136+ahzabayanaka@users.noreply.github.com>
2024-06-08 23:32:20 +0700

    Update main.c, nambahin integrasi fungsi nomor 1 nomor 6 sama pilihan menu

--- main.c
+++ main.c
@@ -4,14 +4,27 @@
 
 int main() {
 
-
     //input bla2
     data data = {NULL, NULL, NULL};
     parse("satu.csv", &data);
     parse("dua.csv", &data);
     parse("tiga.csv", &data);
 
+    //opsi menu ubah aja sesuai kerapihan sama bagusnya gimana nanti
+    printf("Daftar fungsi yang dapat dilakukan:\n1. fungsi no.1\n2. fungsi no.2\n3. fungsi no.3\n4. fungsi no.4\n5. fungsi no.5\n6. fungsi no.6\nPilihan: ");
+    int choice;
+    scanf("%d", &choice);
+    getchar();  // Consume newline
 
+    switch (choice) {
+        case 1: break;
+        case 2: crud_ket_pasien(&data); break;
+        case 3: break;
+        case 4: break;
+        case 5: break;
+        case 6: display_kontrol(&data); break;
+        default: printf("PILIHAN ANDA NGASAL!\n"); break; //sori gakepikiran yang lain
+    }
     
     save("satu.csv", &data);
     save("dua.csv", &data);
