12b2f1bddaa072485845ed5527dc42315d74c3d8
aryaahz <117053291+aryaahz@users.noreply.github.com>
2024-06-13 20:05:55 +0700

    Tambah fungsi 1

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include "display_kontrol.c"
 #include "keuangan_klinik.c"
 #include "functions.c"
+#include "fungsiDataPasien.c"
 
 int main() {
 
@@ -16,12 +17,33 @@ int main() {
 
     //opsi menu ubah aja sesuai kerapihan sama bagusnya gimana nanti
     printf("Daftar fungsi yang dapat dilakukan:\n1. fungsi no.1\n2. fungsi no.2\n3. fungsi no.3\n4. fungsi no.4\n5. fungsi no.5\n6. fungsi no.6\nPilihan: ");
-    int choice;
+    int choice; char string[MAX_LEN];
     scanf("%d", &choice);
     getchar();  // Consume newline
 
     switch (choice) {
-        case 1: break;
+        case 1: 
+            printf("Pilih operasi:\n1. Cari data pasien\n2. Tambah data pasien\n3. Ubah data pasien\n4. Hapus data pasien\nPilihan: ");
+            scanf("%d", &choice);
+            {switch (choice) {
+                case 1: 
+                    printf("Masukkan ID pasien yang ingin dicari: ");
+                    scanf(" %[^\r\n]%*c", string);
+                    search(&data, string); break;
+                case 2: insertEnd(&data); break; //Tambah pasien
+                // case 3: 
+                //     printf("Masukkan ID pasien yang datanya ingin di-update: ");
+                //     scanf(" %[^\r\n]%*c", string);
+                //     updateData(&data, string);
+                //     break;
+                // case 4:
+                //     printf("Masukkan ID pasien yang datanya ingin dihapus: ");
+                //     scanf(" %[^\r\n]%*c", string);
+                //     //Update node satu (first struct in data)
+                //     data = hapusData(&data, string);
+                //     break;
+                }
+            } break;
         case 2: crud_ket_pasien(&data); break;
         case 3: break;
         case 4: laporan_keuangan(); break;
