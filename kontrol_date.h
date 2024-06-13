8d7df9d317c6b69622932968595303397bc494ea
bayanaka <130218136+ahzabayanaka@users.noreply.github.com>
2024-06-14 05:13:07 +0700

    Create kontrol_date.h
    header fungsi buat tanggal kontrol

---
+++ kontrol_date.h
@@ -0,0 +1,10 @@
+#ifndef KONTROL_DATE_H
+#define KONTROL_DATE_H
+
+#include "main.h"
+
+int is_leap_year(int year);
+int days_in_month(int month, int year);
+void add_days(char* input_date, char* result_date);
+
+#endif
