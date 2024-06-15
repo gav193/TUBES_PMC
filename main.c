370288b29bd2c0455f93f7b8caace41d089f6904
gav193 <116613315+gav193@users.noreply.github.com>
2024-06-15 08:15:40 +0700

    Update main.c

--- main.c
+++ main.c
@@ -16,45 +16,60 @@ int main() {
     parse("tiga.csv", &data);
 
     //opsi menu ubah aja sesuai kerapihan sama bagusnya gimana nanti
-    printf("Daftar fungsi yang dapat dilakukan:\n1. fungsi no.1\n2. fungsi no.2\n3. fungsi no.3\n4. fungsi no.4\n5. fungsi no.5\n6. fungsi no.6\nPilihan: ");
-    int choice; char string[MAX_LEN];
-    scanf("%d", &choice);
-    getchar();  // Consume newline
-
-    switch (choice) {
-        case 1: 
-            printf("Pilih operasi:\n1. Cari data pasien\n2. Tambah data pasien\n3. Ubah data pasien\n4. Hapus data pasien\nPilihan: ");
-            scanf("%d", &choice);
-            {switch (choice) {
-                case 1: 
-                    printf("Masukkan ID pasien yang ingin dicari: ");
-                    scanf(" %[^\r\n]%*c", string);
-                    search(&data, string); break;
-                case 2: insertEnd(&data); break; //Tambah pasien
-                case 3: 
-                    printf("Masukkan ID pasien yang datanya ingin di-update: ");
-                    scanf(" %[^\r\n]%*c", string);
-                    updateData(&data, string);
-                    break;
-                case 4:
-                    printf("Masukkan ID pasien yang datanya ingin dihapus: ");
-                    scanf(" %[^\r\n]%*c", string);
-                    //Update node satu (first struct in data)
-                    hapusData(&data, string);
-                    break;
-                }
-            } break;
-        case 2: crud_ket_pasien(&data); break;
-        case 3: break;
-        case 4: laporan_keuangan(); break;
-        case 5: break;
-        case 6: display_kontrol(&data); break;
-        default: printf("PILIHAN ANDA NGASAL!\n"); break; //sori gakepikiran yang lain
-    }
+    do {
+        printf("Daftar fungsi yang dapat dilakukan:\n1. Daftar Pasien\n2. Daftar Riwayat\n3. fungsi no.3\n4. fungsi no.4\n5. fungsi no.5\n6. fungsi no.6\nPilihan: ");
+        int choice; char string[MAX_LEN];
+        scanf("%d", &choice);
+        getchar();  // Consume newline
     
-    save("satu.csv", &data);
-    save("dua.csv", &data);
-    save("tiga.csv", &data);
+        switch (choice) {
+            case 1: 
+                int choice_2 = 0;
+                printf("Pilih operasi:\n1. Cari data pasien\n2. Tambah data pasien\n3. Ubah data pasien\n4. Hapus data pasien\n5. Back\nPilihan: ");
+                scanf("%d", &choice_2);
+                {switch (choice_2) {
+                    case 1: 
+                        printf("Masukkan ID pasien yang ingin dicari: ");
+                        scanf(" %[^\r\n]%*c", string);
+                        search(&data, string); 
+                        break;
+                    case 2: 
+                        insertEnd(&data); 
+                        break; //Tambah pasien
+                    case 3: 
+                        printf("Masukkan ID pasien yang datanya ingin di-update: ");
+                        scanf(" %[^\r\n]%*c", string);
+                        updateData(&data, string);
+                        break;
+                    case 4:
+                        printf("Masukkan ID pasien yang datanya ingin dihapus: ");
+                        scanf(" %[^\r\n]%*c", string);
+                        //Update node satu (first struct in data)
+                        hapusData(&data, string);
+                        break;
+                    case 5: 
+                        printf("Kembali ke halaman awal\n");
+                        break;
+                    default:
+                        printf("Input tidak valid, kembali ke halaman awal\n");
+                        break;
+                    }
+                } break;
+            case 2: crud_ket_pasien(&data); break;
+            case 3: break;
+            case 4: laporan_keuangan(); break;
+            case 5: break;
+            case 6: display_kontrol(&data); break;
+            case 7: 
+                printf("Exiting system...\n");
+                save("satu.csv", &data);
+                save("dua.csv", &data);
+                save("tiga.csv", &data);
+                break;
+            default: printf("Pilihan tidak valid, ulangi input!\n"); break; //sori gakepikiran yang lain
+        }
+    } while (choice != 7);
+    
 
     return 0;
 }
