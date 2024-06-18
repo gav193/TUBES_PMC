3f477dc12db1919ff48a6a7ecf9a6cd14319f1bc
gav193 <116613315+gav193@users.noreply.github.com>
2024-06-18 19:55:32 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,10 @@
-#include "mainp.h"
+#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
 #include <ctype.h>
-#include "functionsp.c"
+#include "functions.c"
 
 int main() {
 
